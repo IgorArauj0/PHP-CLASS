5af09597913cc3b9cb6cb0b0efd470d777ed8a84
Igor Araújo <igoritoaraujo223@gmail.com>
2024-10-30 21:50:26 -0300

    Salvamento

---
+++ aula1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <h1>TESTANDO CÓDIGO EM PHP</h1>
+
+    <?php 
+    
+      $nome = "Igor Araujo";
+       
+      echo "Eu me chamo $nome";
+
+
+    ?>
+
+
+</body>
+</html>
